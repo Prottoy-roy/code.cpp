cf28c79a1d3383a758595abf3028217ebbd05cbc
Prottoy-roy <prottoyroy4@gmail.com>
2025-11-14 18:05:49 +0600

    add bool c++

---
+++ basic.cpp/bool.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+  bool isThis = true; // true - 1
+  bool inThat = false; // false - 0
+  cout << isThis << endl;
+  cout << inThat << endl;
+  return 0;
+}
