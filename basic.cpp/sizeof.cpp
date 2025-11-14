db9252d48467c17e044e5abd3b493ec75596e375
Prottoy-roy <prottoyroy4@gmail.com>
2025-11-14 18:17:30 +0600

    add sizeof c++

---
+++ basic.cpp/sizeof.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int age;
+  double age1;
+  float age2;
+  char age3;
+  bool age4;
+  cout << "int = " << sizeof(age) << endl;
+  cout << "double = " << sizeof(age1) << endl;
+  cout << "float = " << sizeof(age2) << endl;
+  cout << "char = " << sizeof(age3) << endl;
+  cout << "bool = " << sizeof(age4) << endl;
+  return 0;
+}
