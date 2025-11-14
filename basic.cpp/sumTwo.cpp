642efc2431609afecdab01ac5cd18fbd58552487
Prottoy-roy <prottoyroy4@gmail.com>
2025-11-14 18:22:58 +0600

    add sumTwo cpp

---
+++ basic.cpp/sumTwo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int a,b;
+  cout << "Enter a: " <<endl;
+  cin >> a;
+  cout << "Enter b: " <<endl;
+  cin >> b;
+  int sum=(a+b);
+  cout << "sum of two numbers = " << sum <<endl;
+  return 0;
+}
