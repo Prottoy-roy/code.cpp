e113eb377a106d8baa48361fca6e17dc03b32c02
Prottoy-roy <prottoyroy4@gmail.com>
2025-11-17 21:17:19 +0600

    add

---
+++ basic.cpp/while.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n = 20;
+  int i = 1;
+  while(i <= n){
+    cout << i << " ";
+    i++;
+  }
+  cout << endl;
+  return 0;
+}
--- code.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-  cout<<"prottoy" <<"\n" "roy" <<endl;
-  return 0;
-}
